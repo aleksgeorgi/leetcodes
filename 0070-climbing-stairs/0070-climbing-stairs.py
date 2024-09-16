a2b1e63bf4031e25e4cfcd81b709ba0e5d01c827
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-09-16 17:11:59 -0400

    Time: 31 ms (77.84%), Space: 16.6 MB (13.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,23 +1,18 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n==0 or n==1:
-            return 1
+        # decalre a memo to store recursive computations 
+        memo = [0] * (n+1)
+        return self.climb_stairs(0, n, memo)
         
-        '''
-        recurrence relation F(n) = F(n-1) + F(n-2)
+    def climb_stairs(self, i: int, n: int, memo: List[int]) -> int:
+        if i > n:
+            return 0 # you've gone too far 
+        if i == n:
+            return 1 # only one way to get to where you are 
+            
+        if memo[i] > 0:
+            return memo[i]
+            
+        memo[i] = self.climb_stairs(i+1, n, memo) + self.climb_stairs(i+2, n, memo)
         
-        base cases:
-        F(0) = 1
-        F(1) = 1
-        '''
-        
-        prev1, prev2 = 1, 1 # base cases
-        
-        #calculate F(n) iteratively 
-        for i in range(2, n+1):
-            current = prev1 + prev2
-            prev2 = prev1
-            prev1 = current 
-        
-        return prev1
-        
+        return memo[i]
