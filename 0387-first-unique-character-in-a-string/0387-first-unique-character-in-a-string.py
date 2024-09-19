95e83f8927dd002fcd919f54402de8e80d1832f2
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-09-19 15:03:50 -0400

    Time: 115 ms (33.78%), Space: 17.2 MB (30.45%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,25 +1,20 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        # brute force way 
-        seen = set()
+        # hashmap way 
+        seen = {}
         
-        # Outer loop: Iterate over each character
+        #iterate through once to add the letter counts to the hashmap
+        for char in s:
+            if char not in seen:
+                seen[char] = 1
+            else:
+                seen[char] = seen.get(char) + 1
+                
+        # if all vals > 1 return -1 
+        if all( value > 1 for value in seen.values()):
+            return -1 
+        
+        # iterate through string and return the first char that has val of 1
         for i in range(len(s)):
-            if s[i] in seen:
-                continue
-            
-            is_unique = True
-            
-            # Inner loop: Compare the current character with every other character
-            for j in range(len(s)):
-                if i != j and s[i] == s[j]:
-                    is_unique = False
-                    seen.add(s[i])
-                    break
-                    
-            # If no other character matched, it's the first non-repeating character
-            if is_unique:
+            if seen[s[i]] == 1:
                 return i
-
-        # If no unique character is found, return -1
-        return -1
