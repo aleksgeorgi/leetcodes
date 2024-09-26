d4eb3cc71862c008bdefc743481cb4ad48d0666a
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-09-26 15:46:57 -0400

    Time: 350 ms (85.45%), Space: 16.9 MB (83.19%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,65 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter = 0 
+        bottom_edge = 0
+        top_edge = len(grid)-1
+        left_edge = 0 
+        right_edge = len(grid[0])-1
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                # look at a cell
+                if grid[r][c] == 1:
+                    
+                    # check if we're at a boundary 
+                    
+                    # row boundary
+                    if r == bottom_edge:
+                        perimeter += 1
+                    else:
+                        # check down 
+                        if grid[r-1][c] == 0:
+                            perimeter += 1 
+                            
+                    if r == top_edge:
+                        perimeter += 1
+                    else:
+                        # check up
+                        if grid[r+1][c] == 0:
+                            perimeter += 1
+                            
+                    # column boundary 
+                    if c == left_edge:
+                        perimeter += 1
+                    else:
+                        # check left
+                        if grid[r][c-1] == 0:
+                            perimeter += 1
+                            
+                    if c == right_edge:
+                        perimeter += 1
+                    else:
+                        # check right 
+                        if grid[r][c+1] == 0:
+                            perimeter += 1
+                            
+                    
+        return perimeter
+                    
+            
+        
+        '''
+        1. look at a cell 
+            if it's a 1, it's land 
+                look up, down, left, right
+                and if it is a boundary or 0, increment the perimeter 
+            else its water, keep going 
+        2. return the perimeter total 
+        
+        '''
+        
+        
+            
+            
+        
+        return 0 
