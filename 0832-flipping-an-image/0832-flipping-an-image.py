b1487d8050de4d624df07d07b3ab488a4ee511bb
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-10-02 13:28:08 -0400

    Time: 45 ms (82.03%), Space: 16.6 MB (42.22%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.py
+++ 0832-flipping-an-image/0832-flipping-an-image.py
@@ -4,13 +4,7 @@ class Solution:
         rev_list_inv = []
         
         for row in image:
-            rev_list = row[::-1]
-            for i in range(len(rev_list)):
-                if rev_list[i] == 1:
-                    rev_list[i] = 0
-                else:
-                    rev_list[i] = 1
-            rev_list_inv.append(rev_list)
+            rev_list_inv.append(list(map(lambda x: 0 if x==1 else 1, row[::-1])))
                     
         return rev_list_inv
         
