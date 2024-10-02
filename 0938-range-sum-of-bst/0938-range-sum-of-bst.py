071b2af4232c64595726fa70b39e60c173cc12ef
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-10-02 14:16:21 -0400

    Time: 111 ms (65.41%), Space: 23.8 MB (18.33%) - LeetHub

---
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if root is None:
+            return 0
+        
+        valSum = 0
+        
+        if low <= root.val <= high:
+            valSum += root.val
+        
+        # check left subtree for smaller values 
+        if root.val > low:
+            valSum += self.rangeSumBST(root.left, low, high) 
+        
+        if root.val < high:
+            valSum += self.rangeSumBST(root.right, low, high)
+        
+        return valSum
+        
