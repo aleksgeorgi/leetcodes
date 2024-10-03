27aeed1dceef39899095fe407d341b57d9cd2e82
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-10-03 12:03:22 -0400

    Time: 52 ms (18.91%), Space: 16.7 MB (39.15%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -1,12 +1,5 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
         # [phoneNumber(10)+gender(1)+age(2)+seat(2)] total = 15
-        total = 0
-        
-        for person in details:
-            age = person[11] + person[12]
-            if int(age) > 60:
-                total += 1
-                
-        return total
+        return sum(s[11:13] > '60' for s in details)
         
