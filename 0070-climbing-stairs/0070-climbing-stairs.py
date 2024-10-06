c430771812f8689e934b3f9a0005195fb93dfa1d
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-10-06 18:31:44 -0400

    Time: 29 ms (86.96%), Space: 16.5 MB (65.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,18 +1,15 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        # decalre a memo to store recursive computations 
-        memo = [0] * (n+1)
-        return self.climb_stairs(0, n, memo)
-        
-    def climb_stairs(self, i: int, n: int, memo: List[int]) -> int:
-        if i > n:
-            return 0 # you've gone too far 
-        if i == n:
-            return 1 # only one way to get to where you are 
-            
-        if memo[i] > 0:
-            return memo[i]
-            
-        memo[i] = self.climb_stairs(i+1, n, memo) + self.climb_stairs(i+2, n, memo)
-        
-        return memo[i]
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        first = 1
+        second = 2
+        for i in range(3, n + 1):
+            third = first + second
+            first = second
+            second = third
+        return second
