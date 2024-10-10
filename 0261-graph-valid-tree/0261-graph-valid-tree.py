869602cca84ca29a53aa55861d57356aa188d9d1
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-10-10 15:43:27 -0400

    Time: 94 ms (17.48%), Space: 18.4 MB (43.14%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,48 +1,29 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) != n-1:
-            return False
+        if len(edges) != n-1: return False
         
-        adj_list = {i:[] for i in range(n)}
-        # convert the edges into an adjacency list
+        # convert the edge list into adj list
+        adj_list = [[] for _ in range(n)]
+        
         for u,v in edges:
             adj_list[u].append(v)
             adj_list[v].append(u)
-            
-        # check for cycles 
+                
         visited = set()
         
-        # traverse with dfs starting with node 0
-        is_valid_tree = dfs(0, -1, adj_list, visited)
-        
-        # if dfs detects a cycle or not all nodes visted, return false not valid tree
-        if not is_valid_tree:
-            return False
-        
-        # check if all nodes have been visted 
-        if len(visited) != n:
-            return False
-        
-        return True # no cycles and all nodes visited 
-        
-def dfs(node, parent, adj_list, visited):
-    # mark the current node as visited
-    visited.add(node)
+        def dfs(node, parent):
+            if node in visited: return;
+            
+            visited.add(node)
+
+            for neighbor in adj_list[node]:
+                if neighbor == parent:
+                    continue # skip to prevent endless loop 
+                if neighbor in visited:
+                    return False
+                result = dfs(neighbor, node)
+                if not result: return False
+            return True
     
-    # traverse all the neighbors of the current node
-    for neighbor in adj_list[node]:
-        # if the neighbor is the parent, skip it to avoid revisiting the parent endlesslyct
-        if neighbor == parent:
-            continue
-        
-        if neighbor in visited:
-            return False # we found a cycle if we already visited this neighbor 
-    
-        # visit the neighbor recursively and if it returns false, propogate the failure
-        is_valid_tree = dfs(neighbor, node, adj_list, visited)
-        if is_valid_tree == False:
-            return False
-        
-        # if no cycles were found return true
-    return True
-        
+        return dfs(0, -1) and len(visited)==n
+                    
