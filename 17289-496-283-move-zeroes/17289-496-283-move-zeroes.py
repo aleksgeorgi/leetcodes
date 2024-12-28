4354435a171cc59d060b0de1fef42afc03499e9d
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-12-28 18:22:11 -0500

    Time: 0 ms, Memory: 18.9 MB - LeetHub

--- 17289-496-283-move-zeroes/17289-496-283-move-zeroes.py
+++ 17289-496-283-move-zeroes/17289-496-283-move-zeroes.py
@@ -5,16 +5,11 @@ class Solution:
         """
         if len(nums) > 1:
             l = 0
-            r = l+1
-
-            while r < len(nums):
-                if nums[l]==0:
-                    while r < len(nums) and nums[r]==0:
-                        r += 1
-                    if r < len(nums):
-                        nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r = l+1
+            for r in range(len(nums)):
+                if nums[r]!=0 and nums[l]==0:
+                    nums[l], nums[r] = nums[r], nums[l]
+                if nums[l]!=0:
+                    l +=1
             
 
                         
